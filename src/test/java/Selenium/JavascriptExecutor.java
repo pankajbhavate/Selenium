0f7aa523d89e91bc8563eb2264ff56f4de92c9e3
pankaj bhavate <psbhavate9130@gmail.com>
2025-03-02 13:54:17 +0530

    Update JavascriptExecutor.java

--- src/test/java/Selenium/JavascriptExecutor.java
+++ src/test/java/Selenium/JavascriptExecutor.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class JavascriptExecutor {
+public class myjavascriptExecutor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
